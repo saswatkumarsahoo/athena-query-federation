117f36dcffaabc0ef1f1b78a725dd115fa39d2ca
Saswat Sahoo <saswat.kumar.sahoo@randstadgroep.nl>
2023-02-26 15:21:10 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region  = var.region
+  profile = "default"
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_serverlessapplicationrepository_cloudformation_stack" "athena-dynamodb-connector" {
+  name           = "athena-dynamodb-connector"
+  application_id = var.aws_serverlessapplicationrepository_app_id
+  capabilities = [
+    "CAPABILITY_IAM",
+    "CAPABILITY_RESOURCE_POLICY",
+  ]
+  parameters = {
+    AthenaCatalogName = var.AthenaCatalogName
+    SpillBucket       = aws_s3_bucket.athena-spill-bucket.id
+    SpillPrefix       = "dynamodb"
+    LambdaMemory      = 384
+    LambdaTimeout     = 900
+  }
+}
+
+resource "aws_s3_bucket" "athena-spill-bucket" {
+  bucket        = "aws-athena-result-spill-${local.account_id}-${var.region}"
+  force_destroy = true
+}
+
+
+resource "aws_athena_data_catalog" "dyanmodb-catalog" {
+  name        = "dynamodb"
+  description = "Athena data catalog for dynamodb tables"
+  type        = "LAMBDA"
+
+  parameters = {
+    "function" = "arn:aws:lambda:eu-west-1:${local.account_id}:function:${var.AthenaCatalogName}"
+  }
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "AthenaCatalogName" {
+  default = "athena-dynamodb-catalog"
+}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "aws_serverlessapplicationrepository_app_id" {
+  default = "arn:aws:serverlessrepo:us-east-1:292517598671:applications/AthenaDynamoDBConnector"
+}
